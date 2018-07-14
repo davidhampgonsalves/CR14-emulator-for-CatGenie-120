6be2474e4c1ccde6d4168151a74843bf499fe983
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2018-07-14 10:05:55 -0300

    not beeping but will no run

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define SIZEOF(x)  (sizeof (x) / sizeof (x[0]))
+
 #include "Arduino.h"
 #include "Wire.h"
 
@@ -9,12 +11,12 @@
  * Blue - SDL - pin A5
  *
  */
-
 byte I2C_ADDRESS = 80; // 80(7 bits) with a R/W bit appended
 byte CLEAR_PARAMS[] = { 0x00, 0x00 };
 byte ENABLE_CARRIER[] = { 0x00, 0x10 };
 
 byte* readRegister = new byte[4];
+int readRegisterSize;
 
 bool eq(byte d1[], byte d2[], int size) {
   for (int i = 0 ; i < size ; i++) {
@@ -28,6 +30,7 @@ void receiveEvent(int length) {
   //skip single byte write as its setting up the next read
   //and reading the ack breaks the following read
   if(length == 1) {
+    Wire.read();
     return;
   }
 
@@ -42,50 +45,74 @@ void receiveEvent(int length) {
   for(int i = 0 ; Wire.available() ; i++) {
     readRegister[i] = Wire.read();
   }
+  readRegisterSize = length;
 }
 
-byte BLOCK_5[4] = { 0x00, 0x00, 0x00, 0x00 };
-byte BLOCK_6[4] = { 0x00, 0x00, 0x00, 0x00 };
-byte BLOCK_8[4] = { 0x00, 0x3C, 0x00, 0x01 };
-byte BLOCK_9[4] = { 0x00, 0x3C, 0x00, 0x01 };
-byte BLOCK_A[4] = { 0x00, 0x3C, 0x00, 0x01 };
-byte BLOCK_B[4] = { 0x00, 0x00, 0x21, 0x08 };
-byte BLOCK_C[4] = { 0x00, 0x00, 0x21, 0x08 };
-byte BLOCK_D[4] = { 0xCB, 0xD8, 0x2A, 0x30 };
-byte BLOCK_F[4] = { 0xDE, 0xE0, 0x21, 0x08 };
+const byte BLOCK_D[] = { 0x04, 0xF2, 0x81, 0xC7, 0x96 };
+const byte BLOCK_5[] = { 0x04, 0x36, 0x00, 0x00, 0x00 };
+const byte BLOCK_6[] = { 0x04, 0x36, 0x00, 0x00, 0x00 };
+const byte BLOCK_0[] = { 0x04, 0x00, 0x00, 0x00, 0x00 };
+const byte BLOCK_8[] = { 0x04, 0x01, 0x00, 0x78, 0x00 };
+const byte BLOCK_9[] = { 0x04, 0x01, 0x00, 0x78, 0x00 };
+const byte BLOCK_A[] = { 0x04, 0x01, 0x00, 0x78, 0x00 };
+const byte BLOCK_B[] = { 0x04, 0x04, 0x0F, 0x00, 0x00 };
+const byte BLOCK_C[] = { 0x04, 0x04, 0x0F, 0x00, 0x00 };
+const byte BLOCK_F[] = { 0x04, 0x04, 0x0F, 0x7D, 0xF8 };
 
 byte READ_SELECT_COMMAND[] = { 0x01, 0x02, 0x06, 0x00 };
-byte READ_NODE_ID[] = { 0x01, 0x02, 0x0E, 0x45 };
+byte READ_NODE_ID[] = { 0x01, 0x02, 0x0E, 0xD9 };
 byte READ_UID[] = { 0x01, 0x01, 0x0B };
 
-byte NODE_ID_RESPONSE[] = { 0x01, 0x45 };
-byte UID_RESPONSE[] = { 0xD0, 0x02, 0x0D, 0xE4, 0x3F, 0x56, 0x69, 0x67 };
+byte NODE_ID_RESPONSE[] = { 0x01, 0xD9 };
+byte UID_RESPONSE[] = { 0x08, 0x61, 0x18, 0x03, 0x24, 0x01, 0x33, 0x02, 0xD0 };
 
 
+int i = 0;
 // This has to be really fast or we will miss data. Serial prints are too slow
 void requestEvent() {
   byte* response;
   int size = 4;
 
-  if(eq(readRegister, READ_SELECT_COMMAND, sizeof(READ_SELECT_COMMAND)) || eq(readRegister, READ_NODE_ID, sizeof(READ_NODE_ID))) {
+  if(eq(readRegister, READ_SELECT_COMMAND, readRegisterSize) || eq(readRegister, READ_NODE_ID, readRegisterSize)) {
     response = NODE_ID_RESPONSE;
-    size = sizeof(NODE_ID_RESPONSE);
-  } else if(eq(readRegister, READ_UID, sizeof(READ_UID))) {
+    size = SIZEOF(NODE_ID_RESPONSE);
+  } else if(eq(readRegister, READ_UID, readRegisterSize)) {
     response = UID_RESPONSE;
-    size = sizeof(UID_RESPONSE);
+    size = SIZEOF(UID_RESPONSE);
   } else {
     switch(readRegister[3]) {
-      case 0x05: response = BLOCK_5;
-      case 0x06: response = BLOCK_6;
-      case 0x08: response = BLOCK_8;
-      case 0x09: response = BLOCK_9;
-      case 0x0A: response = BLOCK_A;
-      case 0x0B: response = BLOCK_B;
-      case 0x0C: response = BLOCK_C;
-      case 0x0D: response = BLOCK_D;
-      case 0x0F: response = BLOCK_F;
-      default: return;
+      case 0x0D:
+        response = BLOCK_D;
+        break;
+      case 0x05:
+        response = BLOCK_5;
+        break;
+      case 0x06:
+        response = BLOCK_6;
+        break;
+      case 0x00:
+        response = BLOCK_0;
+        break;
+      case 0x08:
+        response = BLOCK_8;
+        break;
+      case 0x09:
+        response = BLOCK_9;
+        break;
+      case 0x0A:
+        response = BLOCK_A;
+        break;
+      case 0x0B:
+        response = BLOCK_B;
+        break;
+      case 0x0C:
+        response = BLOCK_C;
+        break;
+      case 0x0F:
+        response = BLOCK_F;
+        break;
     }
+    size = 5;
   }
 
   Wire.write(response, size);
@@ -96,7 +123,10 @@ void setup() {
 
   Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
+
+  Serial.begin(9600);
 }
 
 void loop() {
+
 }
