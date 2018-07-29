42301c1f09262581b9966076821943b26c7127a1
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2018-07-29 11:52:27 -0300

    start work on cleanup / documentation

---
+++ README.md
@@ -0,0 +1,2 @@
+# Arduino Based CR14 Emulator for CatGenie 120
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include "Wire.h"
 
 /*
+ * Pins - Arduino Uno - A4 (SDA), A5 (SCL) other boards see https://www.arduino.cc/en/Reference/Wire
  *
- * Pins - Arduino Uno A4 (SDA), A5 (SCL) other boards see https://www.arduino.cc/en/Reference/Wire
+ * CatGenie 120 wire colors. This is just a starting point as they like to swap the colors. If this doesn't work you will need to probe them on your device.
  *
  * White - SDA - pin A4
  * Black - SDL - pin A5
@@ -11,9 +12,8 @@
  * Blue - Ground
  *
  */
+
 byte I2C_ADDRESS = 80; // 80(7 bits) with a R/W bit appended
-byte CLEAR_PARAMS[] = { 0x00, 0x00 };
-byte ENABLE_CARRIER[] = { 0x00, 0x10 };
 
 byte* readRegister = new byte[4];
 int readRegisterSize;
@@ -28,24 +28,24 @@ bool eq(byte d1[], byte d2[], int size) {
 }
 
 void receiveEvent(int length) {
-  //skip single byte write as its setting up the next read
-  //and reading the ack breaks the following read
+  // skip single byte write as its setting up the next read
+  // and reading the ack breaks the following read
   if(length == 1) {
-    //hasDecremented = false;
+    // consume data but ignore
     Wire.read();
     return;
   }
 
+  // 2 byte instructions are reset commands and can be ignored
   if(length < 2) {
-    // consume data but ignore
     while(Wire.available()) {
       Wire.read();
     }
     return;
   }
 
+  // 6 byte instructions contain the remaining usage count in byte 4
   if(length > 6) {
-    //hasDecremented = true;
     for(int i = 0 ; Wire.available() ; i++) {
       if(i == 4)
         remaining = Wire.read();
@@ -55,19 +55,17 @@ void receiveEvent(int length) {
     return;
   }
 
+  // all other instructions set the read register for a subsiquent block read (handled by requestEvent)
   for(int i = 0 ; Wire.available() ; i++) {
     readRegister[i] = Wire.read();
   }
   readRegisterSize = length;
 }
 
+// Data that will be "read" from RFID tag by block id
 const byte BLOCK_D[] = { 0x04, 0x1F, 0x7E, 0x30, 0x2A };
 const byte BLOCK_5[] = { 0x04, 0x78, 0x00, 0x00, 0x00 };
 const byte BLOCK_6[] = { 0x04, 0x78, 0x00, 0x00, 0x00 };
-
-const byte DECREMENTED_BLOCK_5[] = { 0x04, 0x77, 0x00, 0x00, 0x00 };
-const byte DECREMENTED_BLOCK_6[] = { 0x04, 0x77, 0x00, 0x00, 0x00 };
-
 const byte BLOCK_0[] = { 0x04, 0x00, 0x00, 0x00, 0x00 };
 const byte BLOCK_8[] = { 0x04, 0x01, 0x00, 0x78, 0x00 };
 const byte BLOCK_9[] = { 0x04, 0x01, 0x00, 0x78, 0x00 };
@@ -79,43 +77,34 @@ const byte BLOCK_F[] = { 0x04, 0x04, 0x0F, 0x2B, 0xFC };
 const byte READ_SELECT_COMMAND[] = { 0x01, 0x02, 0x06, 0x00 };
 const byte READ_NODE_ID[] = { 0x01, 0x02, 0x0E, 0x3C };
 const byte READ_UID[] = { 0x01, 0x01, 0x0B };
-//const byte WRITE_DECREMENT_COUNT = { 0x01, 0x06, 0x09, 0x05, 0x75, 0x00, 0x00, 0x00 };
-//const byte WRITE_DECREMENT_COUNT = { 0x01, 0x06, 0x09, 0x06, 0x75, 0x00, 0x00, 0x00 };
 
 byte NODE_ID_RESPONSE[] = { 0x01, 0x3C };
 byte UID_RESPONSE[] = { 0x08, 0xA3, 0x6E, 0x6A, 0x73, 0x09, 0x33, 0x02, 0xD0 };
 
-
-int i = 0;
-// This has to be really fast or we will miss data. Serial prints are too slow
+// WARNING - This needs to be pretty fast or we will miss data. Serial prints are too slow.
 void requestEvent() {
   byte* response;
   int size = 4;
 
   if(eq(readRegister, READ_SELECT_COMMAND, readRegisterSize) || eq(readRegister, READ_NODE_ID, readRegisterSize)) {
     response = NODE_ID_RESPONSE;
-    size = SIZEOF(NODE_ID_RESPONSE);
+    size = 2;
   } else if(eq(readRegister, READ_UID, readRegisterSize)) {
     response = UID_RESPONSE;
-    size = SIZEOF(UID_RESPONSE);
+    size = 9;
   } else {
+    // Return block requested by previous write to read register
     switch(readRegister[3]) {
       case 0x0D:
         response = BLOCK_D;
         break;
       case 0x05:
-        //if(hasDecremented)
+        // block 5/6 hold the remaining usage count
         response = BLOCK_5;
         response[1] = remaining;
-        //else
-          //response = BLOCK_5;
-        //break;
       case 0x06:
-        //if(hasDecremented)
         response = BLOCK_6;
         response[1] = remaining;
-        //else
-          //response = BLOCK_6;
         break;
       case 0x00:
         response = BLOCK_0;
@@ -150,10 +139,8 @@ void setup() {
 
   Wire.onRequest(requestEvent);
   Wire.onReceive(receiveEvent);
-
-  Serial.begin(9600);
 }
 
 void loop() {
-
+  delay(100);
 }
